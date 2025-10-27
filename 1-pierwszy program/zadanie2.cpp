7c7b0257e67f3a5783533f9afc9730e3de8a53c4
AdamSkorupinski <askorupinski838@technikumkreatywne.pl>
2025-10-27 14:10:51 +0100

    komentarz

---
+++ 1-pierwszy program/zadanie2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a = 5;      // pierwsza liczba
+	int b = 3;      // druga liczba
+	
+	int suma = a + b;
+	int roznica = a - b;
+	int iloczyn = a * b;
+
+	cout << "Liczba a: " << a << endl;
+	cout << "Liczba b: " << b << endl;
+	cout << "Suma: " << suma << endl;
+	cout << "Różnica: " << roznica << endl;
+	cout << "Iloczyn: " << iloczyn << endl;
+	
+	return 0;
+}
