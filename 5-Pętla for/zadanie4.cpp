a6097edd515aec77b53f212282f4fe91706ddd8d
AdamSkorupinski <askorupinski838@technikumkreatywne.pl>
2026-01-12 14:08:06 +0100

    Komentarz

---
+++ 5-Pętla for/zadanie4.cpp
@@ -0,0 +1,31 @@
+  #include <iostream>
+using namespace std;
+ 
+int main()
+{
+   int a;
+   cout <<"Podaj liczbe wierszy: ";
+   cin >> a;
+ 
+   int b;
+   cout <<"Podaj liczbe kolumn: ";
+   cin >> b;
+    
+   cout <<endl;
+ 
+   int c = 1;
+   for(int i =0; i < a; i++)
+   {
+       for(int j = 0; j < b; j++)
+       {
+       
+        cout<<c<<"      ";
+        c+=2;
+       }
+       cout<<endl;
+   }
+   
+   
+   return 0;
+   
+}
