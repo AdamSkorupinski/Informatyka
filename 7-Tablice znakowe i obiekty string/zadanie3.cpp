3532c2aa3cd8c246660ed79aabc6e9fb073ab1f5
AdamSkorupinski <askorupinski838@technikumkreatywne.pl>
2026-04-13 14:10:24 +0200

    zadanie

--- 7-Tablice znakowe i obiekty string/zadanie3.cpp
+++ 7-Tablice znakowe i obiekty string/zadanie3.cpp
@@ -3,16 +3,35 @@ using namespace std;
 int main()
 {
 	char a[21];
+	
 	char znak;
 	cout << "Podaj ciag znakow (max 20 znakow): ";
+	
 	cin.getline(a,21);
 	cout << "Podaj znak do zaliczenia: ";
+	
 	cin >> znak;
 	int licznik = 0;
+	
 	int wystopienia[20];
 	
-	for (int)
+	for (int i = 0; i < strlen(a); i++)
+	{
+		if(a[i] == znak)
+	{
 	
+	wystopienia[licznik] = i;
+	licznik ++;
+	}
+}
+cout << "Liczba wystopien znaku" << znak << " :" << licznik << endl;
+cout << "Pozycje wystapien:";
+for (int i = 0; i < licznik; i++)
+{
+	cout << wystopienia[i]<<",";
+}
+
+
 	  return 0;
 }
  
---
+++ 7-Tablice znakowe i obiekty string/zadanie4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a [21];
+	char odwrot[21];
+	cout << "Podaj ciag znakow (max 20 znakow): ";
+	cin.getline(a,21);
+	int licz = 0;
+	  return 0;
+}
+ 
